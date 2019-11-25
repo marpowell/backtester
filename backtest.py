5eaa25748b9040641c72a5106a7df2570998e6ee
marpowell <marpowll@gmail.com>
2019-11-25 14:04:08 -0600

    Update backtest.py
    Now uses class structure, can optimize parameters

--- backtest.py
+++ backtest.py
@@ -1,31 +1,79 @@
 import data_reader
 import strategies
 
-date, op, hi, lo, cl, adj, vo = data_reader.read_stock_data("AAPL")
-signals = strategies.sma_crossover(date, adj) #this line dictates which strat to use
-	
-equity = 100000 #start with $100,000 cash
-shares = 0
-portfolio = [["Date", "Cash", "Shares of AAPL"]]
-
-for i in range(len(signals)):
-	price = signals[i][2] 
-	if signals[i][1] == "Buy" and shares == 0:    #if there's a buy signal and we have no shares
-		units = int(equity / price)               #buy as many shares as you can
-		units = min(1000, units - (units % 100))  #afford, in increments of 100
-		equity -= units * price                   #with a maximum order of 1000 shares
-		shares = units
-	elif signals[i][1] == "Sell" and shares != 0: #at sell signals, sell all remaining
-		equity += shares * price                  #stock at the current day's price
+class backtest:
+	def __init__(self, data, strategy):
+		self.date = data[0]
+		self.op = data[1]
+		self.hi = data[2]
+		self.lo = data[3]
+		self.cl = data[4]
+		self.adj = data[5]
+		self.vo = data[6]
+		self.signals = strategy(self.date, self.adj)
+		self.holding_period_long = 100
+		self.holding_period_short = 6
+		
+	def calculate_profits(self):
+		signals_copy = list(self.signals)
+		equity = 100000 #start with $100,000 cash
 		shares = 0
-	#this investment strategy is long only--no short selling
-	portfolio.append([signals[i][0], equity, shares]) 		
+		shorts = 0
+		position_size = 1000
+		positions = []
+		portfolio = [["Date", "Account value", "Long shares of AAPL", "Short shares of AAPL"]]
 
-if shares > 0: #if there are any shares leftover on today's date, cash out
-	equity += shares * adj[-1]
-	shares = 0
+		for i in range(len(self.date)):
+			for each in positions:
+				each[2] -= 1
+				if each[2] == 0:
+					if each[0] == "long":
+						shares -= each[1]
+						equity += each[1] * self.adj[i]
+					elif each[0] == "short":
+						shorts -= each[1]
+						equity -= each[1] * self.adj[i] 
+					positions.remove(each)
+			
+			if len(signals_copy) > 1 and self.date[i] == signals_copy[1][0]:
+				if signals_copy[1][1] == "Buy":
+					units = min(position_size, int(equity/self.adj[i]))
+					units -= units % 100
+					shares += units
+					equity -= self.adj[i] * units
+					positions.append(["long", units, self.holding_period_long])
+				elif signals_copy[1][1] == "Sell":
+					units = position_size
+					shorts += units
+					equity += units * self.adj[i]
+					positions.append(["short", units, self.holding_period_short])
+				signals_copy.remove(signals_copy[1])
 	
-portfolio.append([date[-1], equity, 0])
+			portfolio.append([self.date[i], equity + self.adj[i]*shares - 
+				self.adj[i]*shorts, shares, shorts])
+	
+		return portfolio
 
-#portfolio array represents the cash/shares amount at the time of each signal
-print(portfolio[-1])
+	def optimize_holding_period(max_long=100,max_short=10): #very simple, brute force optimization
+		best = [0, 0, 0]
+		for l in range(max_long):
+			for m in range(max_short):
+				self.holding_period_long = l+1
+				self.holding_period_short = m+1
+				profits = calculate_profits()[-1][1] 		
+				if profits > best[2]:
+					best = [l+1, m+1, profits]
+		
+		self.holding_period_long = best[0]
+		self.holding_period_short = best[1]
+		return best
+	
+data = data_reader.read_stock_data("AAPL")
+strategy = strategies.sma_crossover #
+
+
+test = backtest(data, strategy)
+print(test.calculate_profits()[-1]) #calculate profits returns an array of dates and the account value on each date
+
+
+
